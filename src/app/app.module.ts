6e8e37900c7d73dd503974390118b6533ead6242
Luis <luisstemmer2000@gmail.com>
2018-10-26 17:44:29 -0300

    novos arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
+import { ListaProdutosComponent } from './components/home/lista-produtos/lista-produtos.component';
+import { FiltroComponent } from './components/home/filtro/filtro.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ListaProdutosComponent,
+    FiltroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
